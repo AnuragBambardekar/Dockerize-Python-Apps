b4592dfe93db92d281588900cc807d4847471864
AnuragBambardekar <anurag.bambardekar@gmail.com>
2023-07-30 23:19:13 -0400

    Fast API Dockerization - added volumes to persist the data

--- FastAPI_dockerization/src/main.py
+++ FastAPI_dockerization/src/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 def getName():
-    return {"Anurag":"yoloB@mba"}
+    return {"Anurag":"yolodolo"}
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host="0.0.0.0")
